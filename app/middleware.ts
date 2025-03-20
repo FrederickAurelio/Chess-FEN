5e6ea446cfead67415a4a103fd606c3df9324795
Frederick Aurelio Halim <frederick.ah88@gmail.com>
2025-03-20 21:55:10 +0800

    middleware to x-forward

---
+++ app/middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export const middleware = async (req: NextRequest) => {
+  console.debug("REQUEST HEADERS:::: ", req.headers);
+
+  const response = NextResponse.next();
+  response.headers.set(
+    "x-forwarded-host",
+    req.headers.get("origin")?.replace(/(http|https):\/\//, "") || "*"
+  );
+  return response;
+};
--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     serverActions: {
-      allowedOrigins: [
-        "localhost:3000", // localhost
-        "foobar-3000.app.github.dev", // Codespaces
-        "120.26.45.50:6060", // Add your server's IP and port
-      ],
+      allowedOrigins: ["localhost:3000", "120.26.45.50:6060"],
     },
   },
   webpack: (config) => {
