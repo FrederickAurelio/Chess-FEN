98d902bfc2a9a293e0cf1dfed4ec0ee4d39d5c8f
Frederick Aurelio Halim <frederick.ah88@gmail.com>
2025-03-20 21:43:42 +0800

    add allowed origin on next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3000", // localhost
+        "foobar-3000.app.github.dev", // Codespaces
+        "120.26.45.50:6060", // Add your server's IP and port
+      ],
+    },
+  },
   webpack: (config) => {
     // Add custom rules for loading .pb and .json files if necessary
     config.module.rules.push({
